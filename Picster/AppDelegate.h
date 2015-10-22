db8f5e5a0204b5e48b9221406a6e3877835dfd78
Jake Castro <jakedcastro@me.com>
2015-10-22 14:51:03 -0700

    Initial Commit

---
+++ Picster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Picster
+//
+//  Created by Jake Castro on 10/22/15.
+//  Copyright © 2015 Jake Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
