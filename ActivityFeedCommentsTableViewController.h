5889ac83357d4731b0afa8bc470e0ee1e8765579
Jake Castro <Jake@Jakes-MacBook-Pro.local>
2015-10-23 18:44:45 -0700

    added more UI elements

---
+++ ActivityFeedCommentsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActivityFeedCommentsTableViewController.h
+//  Picster
+//
+//  Created by Jake Castro on 10/23/15.
+//  Copyright © 2015 Jake Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityFeedCommentsTableViewController : UITableViewController
+
+@end
---
+++ UserCommentsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserCommentsTableViewController.h
+//  Picster
+//
+//  Created by Jake Castro on 10/23/15.
+//  Copyright © 2015 Jake Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserCommentsTableViewController : UITableViewController
+
+@end
