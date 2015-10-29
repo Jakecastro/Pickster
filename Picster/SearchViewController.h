34497543d42a4df37625a98ad27f69792235e620
Jake Castro <Jake@Jakes-MacBook-Pro.local>
2015-10-29 14:22:21 -0700

    added search storyboard

---
+++ Picster/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  Picster
+//
+//  Created by Jake Castro on 10/29/15.
+//  Copyright © 2015 Jake Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
